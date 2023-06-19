f4c876477ae9c4c2fb9543232c9a40d121b44eb9
scottuzamaki <scottuzamaki@gmail.com>
2023-06-19 12:47:28 -0700

    debug

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 app.get('/', function(req, res) {
-  res.sendFile(views + 'index.html');
+  res.sendFile(myExpressApp + 'index.html');
 });
 
 // view engine setup
