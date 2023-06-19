1fac0ebb0f86b38cd966285f20f4431ded5d62b7
scottuzamaki <scottuzamaki@gmail.com>
2023-06-19 11:12:10 -0700

    hello world

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
